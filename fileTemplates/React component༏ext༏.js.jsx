6b69bc3d75467b48a0efcd70858ec317836daf78
Benjamin Malmgren-Hansen <benjamin@vivino.com>
2020-02-20 16:34:10 +0100

    RM-2018.2.1 <Benjamin@Benjamins-MacBook-Pro-2.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, git.xml, other.xml, Default.xml, Refactor_this.xml, find.xml, ui.lnf.xml, filetypes.xml, code.style.schemes.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, React component༏ext༏.js.jsx, Default.xml, jdk.table.xml, laf.xml, Default _1_.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, keymap.xml, editor.xml, rubysettings.xml, usageView.xml, applicationLibraries.xml, Haml༏ext༏.html.haml, debugger.xml

---
+++ fileTemplates/React component༏ext༏.js.jsx
@@ -0,0 +1,34 @@
+class ${Class_Name} extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    // Event Handlers
+    this.handleInteraction = (e) => {
+      
+    };
+    // Helper Functions
+    this.doAction = () => {
+      
+    };
+  }
+  componentDidMount() {
+  }
+  componentWillReceiveProps(nextProps) {
+  }
+  render() {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+${Class_Name}.defaultProps = {
+};
+
+${Class_Name}.propTypes = {
+  // Required
+  // Function References (These must be always required)
+  // Optional
+}
